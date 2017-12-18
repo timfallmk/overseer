740fb8c48347705b5c426ceb5e01027482c802f6
Tim <tim@vapor.io>
2017-12-18 13:39:12 -0800

    Removed `sanityCheck()` as it returned errorious results
    See https://github.com/vapor-ware/ksync/issues/94 for more details. This change should be reverted, and the original upstream package used when this issue is more properly solved.

--- proc_master.go
+++ proc_master.go
@@ -289,10 +289,10 @@ func (mp *master) fetch() {
 		mp.warnf("failed to run temp binary: %s (%s) output \"%s\"", err, tmpBinPath, tokenOut)
 		return
 	}
-	if tokenIn != string(tokenOut) {
-		mp.warnf("sanity check failed")
-		return
-	}
+	// if tokenIn != string(tokenOut) {
+	// 	mp.warnf("sanity check failed")
+	// 	return
+	// }
 	//overwrite!
 	if err := move(mp.binPath, tmpBinPath); err != nil {
 		mp.warnf("failed to overwrite binary: %s", err)
